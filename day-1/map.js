9f92314c2da9bbb151679346342baf4806f5d43f
Krushnal Bhatti <krushnalbhatti71@gmail.com>
2025-09-29 12:34:21 +0530

    Add files via upload

---
+++ day-1/map.js
@@ -0,0 +1,13 @@
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const cal = numbers.map((x) => x * 2)
+
+console.log(cal)    
+
+
+const vegetables = ["potato","tomato","onion", "carrot", "cabbage"]
+
+const enter = vegetables.map((veg)=> {
+    console.log(veg)
+})
