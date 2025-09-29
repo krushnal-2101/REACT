697b61943fc7cef4b4aab289ef30ef4d6a35b612
Krushnal Bhatti <krushnalbhatti71@gmail.com>
2025-09-29 12:33:19 +0530

    Add files via upload

---
+++ day-1/destructure.js
@@ -0,0 +1,27 @@
+
+
+const vegetables = ["potato","tomato","onion", "carrot", "cabbage"]
+
+console.log(vegetables.at(2))
+
+console.log(vegetables[3])
+
+
+
+const [one , two] = vegetables
+
+console.log(one)
+
+
+
+const person = {
+    name:"",
+    age:21,
+    city:"bvn"
+};
+
+console.log(person.name)
+
+const { name } = person
+
+console.log(name)
