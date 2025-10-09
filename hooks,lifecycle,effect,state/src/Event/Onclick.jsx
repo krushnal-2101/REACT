b34db6c0981ab0c2078c7ea088841f5ebcb7b2c1
Krushnal Bhatti <krushnalbhatti71@gmail.com>
2025-10-09 13:33:26 +0530

    Add files via upload

---
+++ hooks,lifecycle,effect,state/src/Event/Onclick.jsx
@@ -0,0 +1,15 @@
+
+
+import React from "react";
+
+const Onclick = () => {
+
+    const handleClick = () => [
+        alert("onlick button click")
+    ]
+
+    
+    return <button onClick={handleClick}>CLICK</button>
+}
+
+export default Onclick
