0ad7e6156cbdb0dba0f1a9f3557a4f06bdb78c50
Krushnal Bhatti <krushnalbhatti71@gmail.com>
2025-10-15 14:21:21 +0530

    Add files via upload

---
+++ hooks,lifecycle,effect,state/src/Styling/External.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import './External.css'
+
+const External = () => {
+    
+    return(
+
+        <>
+         <form action="">  
+            
+             <input type="email" className="input" placeholder=" enter your email"   />
+
+            <input type="password"  className="input" placeholder="enter your password" />
+
+             <button>LOGIN</button>
+        </form>
+        </>
+    )
+}   
+
+export default External;
