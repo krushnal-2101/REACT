fb42e5f6d77a92097b6c20d0c44c02f942614fd9
Krushnal Bhatti <krushnalbhatti71@gmail.com>
2025-11-13 12:32:37 +0530

    Add files via upload

---
+++ to-do-list/src/components/Stats.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ListTodo = ({todo, editTodo, deleteTodo, toggleCompleted}) => {
+    <>
+        <table>
+            <thead>
+                <tr>
+                    <th>status</th>
+                </tr>
+            </thead>
+        </table>
+    </>
+}
