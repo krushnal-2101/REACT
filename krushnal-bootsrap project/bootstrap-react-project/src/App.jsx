0b7b17d5ad811e07721fd18ef85d6eab94c5e328
Krushnal Bhatti <krushnalbhatti71@gmail.com>
2025-11-24 13:58:13 +0530

    Add files via upload

---
+++ krushnal-bootsrap project/bootstrap-react-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "./components/Navbar";
+import Slider from "./components/Slider";
+
+const App = () => {
+  return (
+   <>
+    <Navbar/>
+    <Slider/>
+   </>
+  )
+}
+
+export default App
